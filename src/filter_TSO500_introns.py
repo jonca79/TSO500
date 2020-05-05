dcdaa1012dc8fc78fae7ff23330780b106de0bb0
Jonas Almlöf <jonas.almlof@igp.uu.se>
2020-05-05 15:05:05 +0200

    Changed intron filtering accoring to refseq exons

--- src/filter_TSO500_introns.py
+++ src/filter_TSO500_introns.py
@@ -4,23 +4,30 @@ import sys
 
 invcf = sys.argv[1]
 
-inbed = open("DATA/TST500C_manifest.bed")
+inbed1 = open("DATA/TSO500_exon_regions.txt")
+inbed2 = open("DATA/TST500C_manifest.bed")
 
 exon_dict = {}
-for line in inbed :
+for line in inbed1 :
+    lline = line.strip().split("\t")
+    chrom = lline[0]
+    start_pos = int(lline[1])
+    end_pos = int(lline[2])
+    if chrom not in exon_dict :
+        exon_dict[chrom] = []
+    exon_dict[chrom].append([start_pos, end_pos])
+
+for line in inbed2 :
     lline = line.strip().split("\t")
     chrom = lline[0]
     start_pos = int(lline[1])
     end_pos = int(lline[2])
     region = lline[3]
-    #Skip all variants outside exons but keep all in MET and TERT promoter
-    if not (region.find("Exon") != -1 or region.find("MET") != -1 or region.find("TERT_Promoter") != -1) :
-        continue
-    if region.find("Exon") != -1 and (region.find("Additional") != -1 or region.find("Fusion") != -1 or region.find("Amp") != -1) :
-        continue
-    if chrom not in exon_dict :
-        exon_dict[chrom] = []
-    exon_dict[chrom].append([start_pos, end_pos])
+    #Add all variants in MET and TERT promoter
+    if region.find("MET") != -1 or region.find("TERT_Promoter") != -1) :
+        if chrom not in exon_dict :
+            exon_dict[chrom] = []
+        exon_dict[chrom].append([start_pos, end_pos])
 
 outpath = ""
 for folder in invcf.split("/")[:-1] :
